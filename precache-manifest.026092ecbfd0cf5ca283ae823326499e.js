c3b6e01fa359ff96aff3b84447b614137cbcea75
Yuriy13202 <ascendo@list.ru>
2020-11-02 18:56:02 +0700

    Updates

---
+++ precache-manifest.026092ecbfd0cf5ca283ae823326499e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f76ee811da9a7c654bf1f1e8b99260",
+    "url": "/realworld/index.html"
+  },
+  {
+    "revision": "35da43d47dfc20a5badf",
+    "url": "/realworld/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "bde5682f53046b70c6a1",
+    "url": "/realworld/static/css/main.72ff5976.chunk.css"
+  },
+  {
+    "revision": "35da43d47dfc20a5badf",
+    "url": "/realworld/static/js/2.e17029f8.chunk.js"
+  },
+  {
+    "revision": "359147aafa1b0a23b1915b7ef82c9a75",
+    "url": "/realworld/static/js/2.e17029f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde5682f53046b70c6a1",
+    "url": "/realworld/static/js/main.c203e647.chunk.js"
+  },
+  {
+    "revision": "11402a64e43559139f73",
+    "url": "/realworld/static/js/runtime-main.74d00d65.js"
+  },
+  {
+    "revision": "cb284d33f7083fafe1623bf979648873",
+    "url": "/realworld/static/media/attention.cb284d33.png"
+  },
+  {
+    "revision": "b982534d2bde43ceefdbee280a705b35",
+    "url": "/realworld/static/media/check.b982534d.svg"
+  },
+  {
+    "revision": "28856df80ef5f56cf28ecd603836d19b",
+    "url": "/realworld/static/media/heart-fill.28856df8.svg"
+  },
+  {
+    "revision": "d17fcd0b93ec5896bd0de49f03bdcb08",
+    "url": "/realworld/static/media/heart.d17fcd0b.svg"
+  },
+  {
+    "revision": "1197b2932237119bb89d3db10c83b2aa",
+    "url": "/realworld/static/media/user.1197b293.svg"
+  }
+]);
